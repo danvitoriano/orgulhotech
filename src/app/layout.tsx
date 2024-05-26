b60b0003c32212354c3e58a747a7804663baae3d
Dan Vitoriano <vitoriano08@gmail.com>
2024-05-26 12:04:03 -0300

    add script rd station metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="AW-11309461517" />
+      <Script
+          async 
+          src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/21db23b6-9557-4dd7-bcb5-62eea936f818-loader.js"
+        />
     </html>
   )
 }
